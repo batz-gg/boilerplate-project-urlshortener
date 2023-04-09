e387fd41405982e1005cb1a90eaea8adb08f2a7e
batz-gg <batz.gg@gmail.com>
2023-04-09 20:37:06 +0800

    modified:   .gitignore 	modified:   index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const dns = require('dns');
+const url = require('url');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -18,6 +21,43 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+let urlDatabase = [];
+
+app.post('/api/shorturl', (req, res) => {
+  const originalUrl = req.body.url;
+  const parsedUrl = url.parse(originalUrl);
+
+  if (!parsedUrl.host || !parsedUrl.protocol) {
+    res.json({ error: 'invalid url' });
+    return;
+  }
+
+  dns.lookup(parsedUrl.host, (err) => {
+    if (err) {
+      res.json({ error: 'invalid url' });
+      return;
+    }
+
+    const shortUrl = urlDatabase.length;
+    urlDatabase.push(originalUrl);
+
+    res.json({ original_url: originalUrl, short_url: shortUrl });
+  });
+});
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const shortUrl = parseInt(req.params.short_url);
+
+  if (shortUrl >= 0 && shortUrl < urlDatabase.length) {
+    res.redirect(urlDatabase[shortUrl]);
+  } else {
+    res.status(404).send('Not found');
+  }
+});
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
